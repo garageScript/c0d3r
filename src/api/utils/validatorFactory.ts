5ae5a4cf354c33e2cd818be40b37c9eaa2a3d676
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2021-07-03 11:22:47 +0530

    Create validators for schemas

---
+++ src/api/utils/validatorFactory.ts
@@ -0,0 +1,11 @@
+import { ZodSchema } from "zod";
+import { RequestHandler } from "express";
+
+export const validatorFactory = (schema: ZodSchema<any>): RequestHandler => (
+  req,
+  res,
+  next
+) => {
+  req.body = schema.parse(req.body);
+  next();
+};
