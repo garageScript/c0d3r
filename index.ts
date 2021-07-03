3068512b721f365263a271243b8c94c4542ed00b
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2021-07-03 11:21:54 +0530

    Refactor to separate out express app

--- index.ts
+++ index.ts
@@ -2,23 +2,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-import express from "express";
-import { api } from "./src/api";
 import { config } from "./config";
 
 import Bot from "./src/Bot";
+import { app } from "./app";
 export const bot = new Bot();
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send('<a href="https://c0d3.com">Become a software engineer!</a>');
-});
-
-app.use("/api", api);
 
 bot.login(config.discordToken).then(() => console.info("🤖 Bot is logged in"));
+
 app.listen(config.port, () => {
   console.info(`🤖 Bot is listening at :${config.port}`);
 });
