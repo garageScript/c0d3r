d63d3475be8e11e921460ec2d500afd198ffbef7
Flacial <ruthemiumwithdiamond@protonmail.com>
2022-04-01 00:16:03 +0400

    feat(bot): initalize command registrators calls

--- index.ts
+++ index.ts
@@ -6,11 +6,13 @@ import { config } from "./config";
 
 import Bot from "./src/Bot";
 import { app } from "./app";
-import { registerEvents } from "./src/Bot/events";
+import { registerCommands, registerCommandsReplies, registerEvents } from "./src/Bot/events";
 export const bot = new Bot();
 
 bot.login(config.discordToken).then(() => console.info("🤖 Bot is logged in"));
 
+registerCommands();
+registerCommandsReplies();
 registerEvents();
 
 app.listen(config.port, () => {
