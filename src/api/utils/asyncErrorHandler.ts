4667bf848937d3611b88dd7be7dbd74c84114541
Jasir Zaeem <20666236+JasirZaeem@users.noreply.github.com>
2023-04-16 20:34:27 +0530

    Remove unnecessary type annotation

--- src/api/utils/asyncErrorHandler.ts
+++ src/api/utils/asyncErrorHandler.ts
@@ -1,17 +1,6 @@
 import { RequestHandler } from "express";
-import {
-  NextFunction,
-  ParamsDictionary,
-  Query,
-  Request,
-  Response,
-} from "express-serve-static-core";
+import { NextFunction, Request, Response } from "express-serve-static-core";
 
 export const asyncErrorHandler =
-  (fn: RequestHandler) =>
-  (
-    req: Request<ParamsDictionary, any, any, Query>,
-    res: Response,
-    next: NextFunction
-  ) =>
+  (fn: RequestHandler) => (req: Request, res: Response, next: NextFunction) =>
     Promise.resolve(fn(req, res, next)).catch(next);
